b345783d097d4136969ea8df58abe308410639ec
Kyoko <azurasieghart@hotmail.com>
2024-08-27 20:03:46 -0300

    Crating menu interations Creating ABC class Creating Factory Level 1 Bg implementation: parallax

--- code/Const.py
+++ code/Const.py
@@ -1,7 +1,27 @@
-#C
+# C
 COLOR_ORANGE = (255, 128, 0)
+COLOR_RED = (255, 0, 10)
+COLOR_WHITE = (230, 230, 230)
+COLOR_YELLOW = (252, 250, 110)
+COLOR_GREEN = (0, 128, 0)
+COLOR_CYAN = (0, 128, 128)
 
-#W
+# W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
 
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITION',
+               'SCORE',
+               'EXIT')
+
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6, }
